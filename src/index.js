844ddc69fbc1486ab7bc1bf750e9139dbe854ed6
Aldon <aisenberg@freightview.com>
2018-02-14 19:57:57 -0600

    update linting errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './index.css';
 import numeral from 'numeral';
 
+/* eslint-disable no-debugger    */
+/* eslint-disable no-console     */
+
 const myValue = numeral(1000).format('$0,0.00');
-// debugger;
+debugger; //https://github.com/AldonIsenberg11/Dev-Environment-Template/issues/23
 console.log(`You should pay ${myValue} for my services!`);
